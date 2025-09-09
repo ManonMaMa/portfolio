e01cf9e0508fb301bc3bee855bfd739e0fcb2a23
MANON MA <infini030303@gmail.com>
2025-09-09 15:23:06 +0200

    1er commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <!-- appelation des fichiers css -->
+    <link rel="stylesheet" href="pf_fichierscss/pf_accueil.css">
+</head>
+<body>
+    <div class="fondtel">
+        <h1>Mon portfolio</h1>
+
+        <!-- animation du chargement -->
+        <div class="loader">
+            <span class="loader__element un"></span>
+            <span class="loader__element deux"></span>
+            <span class="loader__element trois"></span>
+            <span class="loader__element quatre"></span>
+            <span class="loader__element cinq"></span>
+            <span class="loader__element six"></span>
+            <span class="loader__element sept"></span>
+            <span class="loader__element huit"></span>
+            <span class="loader__element neuf"></span>
+            <span class="loader__element dix"></span>
+            <span class="loader__element onze"></span>
+            <span class="loader__element douze"></span>
+        </div>
+    </div>
+</body>
+</html>
